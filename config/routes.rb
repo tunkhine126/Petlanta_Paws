17facd97b0a5fce35085156a59b135edee6c933f
Will Jones <willjones2010@gmail.com>
2019-05-28 11:39:36 -0400

    adding in session controller, new page with login information, routes for login and logout. Following along lecture at 29:05

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :user
-resources :adoptions
-resources :pets 
+  resources :user
+  resources :adoptions
+  resources :pets
+
+  get '/login', to: "sessions#new", as: 'login'
+  post '/login', to: "sessions#create"
+  delete 'logout', to: "sessions#destroy", as: 'logout'
+
+
+
+
 end
