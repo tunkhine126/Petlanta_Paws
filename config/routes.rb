c999cb306b51497155f3828f4b3d23c5e440b3e2
Will Jones <willjones2010@gmail.com>
2019-05-30 09:03:43 -0400

    added rough pet dance party

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   get '/login', to: "sessions#new", as: 'login'
   post '/login', to: "sessions#create"
   get 'logout', to: "sessions#destroy", as: 'logout'
+  get 'danceparty', to: "pets#danceparty"
 
 end
