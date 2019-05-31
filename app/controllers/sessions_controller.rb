124bd9bc586cc978f77b0316397f510590177563
tunkhine126 <tunkhine126@gmail.com>
2019-05-31 13:36:51 -0400

    NearTheEnd

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
      @user = User.find_by(username: params[:username])
      if @user && @user.authenticate(params[:password])
        session[:user_id] = @user.id
-       redirect_to user_path(@user), notice: "Successfully logged in"
+       redirect_to "/home"
      else
        @error = "Login unsuccessful... No dice."
        render :new
